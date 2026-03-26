414e3c803080f193d93d932d6719a38089054fdf
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-03-26 18:02:47 -0300

    Add variancia e inicio da regra de 3

--- Calculator-20260319T194312Z-1-001/esta.js
+++ Calculator-20260319T194312Z-1-001/esta.js
@@ -1,13 +1,15 @@
 function esta(){
-    let tipo = parseInt(prompt("Digite\n[1] para média\n[2] para mediana\n[3] para moda\n"));
+    let tipo = parseInt(prompt("Digite\n[1] para média\n[2] para mediana\n[3] para moda\n[4] para variancia"));
     let tam = parseInt(prompt("Digite o tamanho da lista: "));
     let soma = 0, meio = 0, par = 0, moda = 0, x = 0;
     let lista = new Array(tam);
     
-    for(cont = 0; cont < lista.length; cont++){
-        lista [cont] = parseFloat(prompt("Digite um número: "));
-        soma += lista [cont];
-    }    
+    if(tipo != 4){
+        for(cont = 0; cont < lista.length; cont++){
+            lista [cont] = parseFloat(prompt("Digite um número: "));
+            soma += lista [cont];
+        }    
+    }
 
     switch(tipo){
         case 1:
@@ -42,5 +44,27 @@ function esta(){
             }
             alert("Moda = ", moda);
             break;
+        case 4: 
+            let valor = new Array(tam);
+            soma = 0;
+            for (let contador = 0; contador < valor.length; contador++) {
+                valor[contador] = parseInt(prompt("Digite um valor: "));
+                soma += valor[contador];
+            }
+            media1 = soma / valor.length;
+            soma = 0;
+            
+            for (let contador = 0; contador < valor.length; contador++) {
+                let menos = valor[contador] - media1;
+                let quadrado = menos * menos;
+                                soma += quadrado;
+            }
+            let variancia = soma / valor.length;
+            alert(variancia);
+            
+            //desvio padrao
+            
+            let desvio = variancia ** 0.5;
+            alert("Desvio padrao" + desvio);
     }
 }
--- Calculator-20260319T194312Z-1-001/regra3.js
+++ Calculator-20260319T194312Z-1-001/regra3.js
@@ -1,3 +1,18 @@
 function reg3(){
-   let escolha = parseInt(prompt());
+   let escolha = parseInt(prompt("Digite\n[1] para regra de 3 simples\n[2] para regra de 3 composta\n"));
+   let a, b, c;
+
+   switch(escolha){
+      case 1:
+         b = parseInt(prompt("Digite o valor: "));
+         a = parseInt(prompt("Digite o valor: "));
+         c = parseInt(prompt("Digite o valor: "));
+
+
+         break;
+      case 2: 
+         b = parseInt(prompt("Digite o valor: "));
+         a = parseInt(prompt("Digite o valor: "));
+         c = parseInt(prompt("Digite o valor: "));
+   }
 }
