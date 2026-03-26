5186188b6170c779e0e0e889cba43c22add41fc7
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-03-26 19:02:55 -0300

    porcentagem update

--- Calculator-20260319T194312Z-1-001/Calculator/porcentagem.js
+++ Calculator-20260319T194312Z-1-001/Calculator/porcentagem.js
@@ -1,3 +1,7 @@
 function porc(){
-    
+    let porcentagem = parseInt(prompt("Digite o valor em porcentagem sem o simbulo (%): "));
+    let num = parseFloat(prompt("Valor para a saber a porcentagem: "));
+
+    let resultado = num * (porcentagem/100);
+    alert(resultado);
 }
