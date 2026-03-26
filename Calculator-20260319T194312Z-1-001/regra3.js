4b9ee39fa0f0f81569d9340a295ccf6204bb687b
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-03-26 18:46:29 -0300

    Regra de 3 pronta

--- Calculator-20260319T194312Z-1-001/regra3.js
+++ Calculator-20260319T194312Z-1-001/regra3.js
@@ -1,18 +1,25 @@
 function reg3(){
    let escolha = parseInt(prompt("Digite\n[1] para regra de 3 simples\n[2] para regra de 3 composta\n"));
-   let a, b, c;
+   let x1, x2, y1, y2, z1, z2;
 
    switch(escolha){
       case 1:
-         b = parseInt(prompt("Digite o valor: "));
-         a = parseInt(prompt("Digite o valor: "));
-         c = parseInt(prompt("Digite o valor: "));
+         x1 = parseInt(prompt("Digite o valor do x1: "));
+         y1 = parseInt(prompt("Digite o valor do y1: "));
+         x2 = parseInt(prompt("Digite o valor do x2: "));
 
-
+         y2 = (x2 / x1) * y1;
+         alert(y2);
          break;
       case 2: 
-         b = parseInt(prompt("Digite o valor: "));
-         a = parseInt(prompt("Digite o valor: "));
-         c = parseInt(prompt("Digite o valor: "));
+         x1 = parseInt(prompt("Digite o valor do x1: "));
+         y1 = parseInt(prompt("Digite o valor do y1: "));
+         z1 = parseInt(prompt("Digite o valor do z1: "));
+         x2 = parseInt(prompt("Digite o valor do x2: "));
+         y2 = parseInt(prompt("Digite o valor do y2: "));
+
+         z2 = ((x2 / x1) * (y2 / y1)) * z1;
+         alert(z2);
+         break;
    }
 }
